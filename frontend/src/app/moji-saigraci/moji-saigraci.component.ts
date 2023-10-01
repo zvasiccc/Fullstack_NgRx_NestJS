9ea4547b8518d1d94037c678bb84420dc3b9fe4b
Zeljko Vasic <zeljadelija01@gmail.com>
2023-10-01 20:51:12 +0200

    com

--- frontend/src/app/moji-saigraci/moji-saigraci.component.ts
+++ frontend/src/app/moji-saigraci/moji-saigraci.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Igrac } from '../shared/models/igrac';
 import { IgracService } from '../services/igrac/igrac.service';
+import { Organizator } from '../shared/models/organizator';
+import { selectPrijavljeniKorisnik } from '../shared/state/korisnik/korisnik.selector';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-moji-saigraci',
@@ -11,12 +14,18 @@ import { IgracService } from '../services/igrac/igrac.service';
 })
 export class MojiSaigraciComponent implements OnInit {
   mojiSaigraci$: Observable<Igrac[]>;
-
+  trenutnoPrijavljeniKorisnik$: Observable<Igrac | Organizator | undefined> =
+    new Observable();
+  //todo da dugme vidi saigrace samo ako prijavljeni igrac ucestvuje na tom turniru
   constructor(
     private route: ActivatedRoute,
-    private igracService: IgracService
+    private igracService: IgracService,
+    private store: Store
   ) {
     this.mojiSaigraci$ = new Observable<Igrac[]>();
+    this.trenutnoPrijavljeniKorisnik$ = this.store
+      .select(selectPrijavljeniKorisnik)
+      .pipe(map((p: any) => p?.prijavljeniKorisnik));
   }
 
   ngOnInit() {
@@ -28,5 +37,9 @@ export class MojiSaigraciComponent implements OnInit {
       // za dohvaćanje igrača na temelju tih parametara.
       this.mojiSaigraci$ = this.igracService.vidiSaigrace(turnirId, igracId);
     });
+  }
+  izbaciIgracaIzTima(igracId: number) {}
+  jeVodjaTima(user: any): user is Igrac {
+    return user.role === 'igrac' && user.vodjaTima === true;
   }
 }
