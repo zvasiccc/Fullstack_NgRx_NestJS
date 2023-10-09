16de2fbc7b990833442db86c14c065fcf49033b3
Zeljko Vasic <zeljadelija01@gmail.com>
2023-10-09 13:18:07 +0200

    finalniProjekat

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { Igrac } from '../shared/models/igrac';
 import { Organizator } from '../shared/models/organizator';
 import * as KorisnikActions from '../shared/state/korisnik/korisnik.actions';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, of } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -29,29 +30,43 @@ export class LoginComponent {
       this.lozinka
     );
     let jwtToken: any;
-    tokenObservable.subscribe(async (token: any) => {
-      jwtToken = token.access_token;
-      let korisnik: Igrac | Organizator | undefined = token.korisnik;
+    tokenObservable
+      .pipe(
+        catchError((error) => {
+          this._snackBar.open(
+            'Pogresno korisnicko ime ili lozinka',
+            'zatvori',
+            {
+              duration: 4000,
+            }
+          );
+          //console.error('Pogresno korisnicko ime ili lozinka', error);
+          return of(null);
+        })
+      )
+      .subscribe(async (token: any) => {
+        jwtToken = token.access_token;
+        let korisnik: Igrac | Organizator | undefined = token.korisnik;
 
-      if (korisnik) {
-        this.prijavljeniKorisnik =
-          korisnik.role == 'igrac'
-            ? (korisnik as Igrac)
-            : (korisnik as Organizator);
-      }
-      if (this.prijavljeniKorisnik) {
-        this.store.dispatch(
-          KorisnikActions.postaviPrijavljenogKorisnika({
-            prijavljeniKorisnik: this.prijavljeniKorisnik,
-          })
-        );
-        this.store.dispatch(
-          KorisnikActions.postaviTokenPrijavljenogKorisnika({
-            token: jwtToken,
-          })
-        );
-      }
-      this.router.navigateByUrl('');
-    });
+        if (korisnik) {
+          this.prijavljeniKorisnik =
+            korisnik.role == 'igrac'
+              ? (korisnik as Igrac)
+              : (korisnik as Organizator);
+        }
+        if (this.prijavljeniKorisnik) {
+          this.store.dispatch(
+            KorisnikActions.postaviPrijavljenogKorisnika({
+              prijavljeniKorisnik: this.prijavljeniKorisnik,
+            })
+          );
+          this.store.dispatch(
+            KorisnikActions.postaviTokenPrijavljenogKorisnika({
+              token: jwtToken,
+            })
+          );
+        }
+        this.router.navigateByUrl('');
+      });
   }
 }
